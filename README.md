fcf0a0a84a012648b43de1fa40aad23942586bf8
nullptr <ijwu345@gmail.com>
2025-02-06 21:58:46 -0800

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+#Install
+sudo pacman -S xorg-xinput xorg-xhost
+
+and then just pip install python things 
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import pyautogui
 import time
-
 import re
 
-def type_with_delay(text, delay=0.01):  
-    words = re.split(r"([\n])", text) 
+def type_with_delay(text, delay=0.5):
+    words = re.split(r"([\n])", text)
     
     for word in words:
         if word == "\n":
-            
-            time.sleep(2) 
-        pyautogui.write(word) 
-        time.sleep(delay)  
+            time.sleep(2)  
+        pyautogui.write(word, interval=0.001)  # interval between each char
+        time.sleep(delay)
 
 print("Switch to your Google Doc window...")
 time.sleep(5)
