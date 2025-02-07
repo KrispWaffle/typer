9a99d1ba6bcaaf14515a61b3a7241cb0beff49cf
Kyle <70916412+KrispWaffle@users.noreply.github.com>
2025-02-06 20:05:39 -0800

    f

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import pyautogui
 import time
 
+import re
 
-
-def type_with_delay(text, delay=0.0002):
-    for character in text:
-        if character == "\n":
+def type_with_delay(text, delay=0.01):  
+    words = re.split(r"([\n])", text) 
+    
+    for word in words:
+        if word == "\n":
             
-            time.sleep(.5)
-       
-        pyautogui.write(character)
+            time.sleep(2) 
+        pyautogui.write(word) 
         time.sleep(delay)  
-       
 
-print("Switch to your Google Doc window")
+print("Switch to your Google Doc window...")
 time.sleep(5)
 
-with open("./test.txt", "r") as file:
+with open("test.txt", "r") as file:
     text = file.read()
 
 type_with_delay(text)
